5d2b49702947003907ff201fa35dbea24bb3bbc2
fengsam <404395182@qq.com>
2017-07-28 17:57:13 +0800

    心情模块可以添加图片

--- app/src/main/java/com/huadi/android/ainiyo/activity/ModeAddingActivity.java
+++ app/src/main/java/com/huadi/android/ainiyo/activity/ModeAddingActivity.java
@@ -1,15 +1,69 @@
 package com.huadi.android.ainiyo.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import com.huadi.android.ainiyo.MainActivity;
+import com.huadi.android.ainiyo.adapter.ImageAdapter;
+import com.donkingliang.imageselector.utils.ImageSelectorUtils;
 
 import com.huadi.android.ainiyo.R;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.lidroid.xutils.view.annotation.event.OnClick;
+import java.util.ArrayList;
+
 
 public class ModeAddingActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 0x00000011;
+
+    private RecyclerView rvImage;
+    private ImageAdapter mAdapter;
+    private Button btn_limit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode_adding);
+
+        ViewUtils.inject(this);
+
+        rvImage = (RecyclerView) findViewById(R.id.rv_image);
+        rvImage.setLayoutManager(new GridLayoutManager(this, 3));
+        mAdapter = new ImageAdapter(this);
+        rvImage.setAdapter(mAdapter);
+
     }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && data != null) {
+            ArrayList<String> images = data.getStringArrayListExtra(ImageSelectorUtils.SELECT_RESULT);
+            mAdapter.refresh(images);
+        }
+    }
+
+
+    @OnClick({R.id.mode_adding_back,R.id.mode_add_pic})
+    public void onClick(View v)
+    {
+        switch (v.getId()) {
+            case R.id.mode_adding_back:
+                startActivity(new Intent(ModeAddingActivity.this,MainActivity.class));
+                break;
+            case R.id.mode_add_pic:
+                ImageSelectorUtils.openPhoto(ModeAddingActivity.this, REQUEST_CODE, false, 9);
+            break;
+        }
+    }
+
 }
---
+++ imageselector/src/main/java/com/donkingliang/imageselector/adapter/ImagePagerAdapter.java
@@ -0,0 +1,83 @@
+package com.donkingliang.imageselector.adapter;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.donkingliang.imageselector.entry.Image;
+import com.photoview.PhotoView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImagePagerAdapter extends PagerAdapter {
+
+    private Context mContext;
+    private List<PhotoView> viewList = new ArrayList<>(4);
+    List<Image> mImgList;
+    private OnItemClickListener mListener;
+
+    public ImagePagerAdapter(Context context, List<Image> imgList) {
+        this.mContext = context;
+        createImageViews();
+        mImgList = imgList;
+    }
+
+    private void createImageViews() {
+        for (int i = 0; i < 4; i++) {
+            PhotoView imageView = new PhotoView(mContext);
+            viewList.add(imageView);
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mImgList == null ? 0 : mImgList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        if(object instanceof  PhotoView){
+            PhotoView view = (PhotoView)object;
+            view.setImageDrawable(null);
+            viewList.add(view);
+            container.removeView(view);
+        }
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position) {
+        final PhotoView currentView = viewList.remove(0);
+        final Image image = mImgList.get(position);
+        container.addView(currentView);
+        Glide.with(mContext).load(new File(image.getPath()))
+                .diskCacheStrategy(DiskCacheStrategy.NONE).into(currentView);
+        currentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mListener != null) {
+                    mListener.onItemClick(position, image);
+                }
+            }
+        });
+        return currentView;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener l) {
+        mListener = l;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position, Image image);
+    }
+}
