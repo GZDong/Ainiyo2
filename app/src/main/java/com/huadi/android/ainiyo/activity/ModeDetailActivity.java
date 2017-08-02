a0d3485986a591c4d725a516bb95a6abd211cedd
fengsam <404395182@qq.com>
2017-08-02 16:10:41 +0800

    心情模块卡片式布局的实现

--- app/src/main/java/com/huadi/android/ainiyo/activity/ModeDetailActivity.java
+++ app/src/main/java/com/huadi/android/ainiyo/activity/ModeDetailActivity.java
@@ -1,11 +1,14 @@
 package com.huadi.android.ainiyo.activity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SimpleItemAnimator;
 
+import com.donkingliang.imageselector.entry.Image;
 import com.huadi.android.ainiyo.R;
 import com.huadi.android.ainiyo.adapter.ImageAdapter;
 import com.huadi.android.ainiyo.entity.ModeInfo;
@@ -16,6 +19,7 @@ public class ModeDetailActivity extends AppCompatActivity {
 
     private RecyclerView rvImage;
     private ImageAdapter mAdapter;
+    private GridLayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +27,37 @@ public class ModeDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_mode_detail);
 
         rvImage = (RecyclerView) findViewById(R.id.rv_mode_image);
-        rvImage.setLayoutManager(new GridLayoutManager(this, 3));
+
+
+        // 判断屏幕方向
+        Configuration configuration = getResources().getConfiguration();
+        if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            mLayoutManager = new GridLayoutManager(this, 3);
+        } else {
+            mLayoutManager = new GridLayoutManager(this, 5);
+        }
+        rvImage.setLayoutManager(mLayoutManager);
+
+
         mAdapter = new ImageAdapter(this);
         rvImage.setAdapter(mAdapter);
         Intent intent=getIntent();
-        ArrayList<String> mi=intent.getStringArrayListExtra("item");
-        mAdapter.refresh(mi);
+        ArrayList<String> mPhoList=intent.getStringArrayListExtra("item");
+        mAdapter.refresh(mPhoList);
+
+//        ((SimpleItemAnimator) rvImage.getItemAnimator()).setSupportsChangeAnimations(false);
+//
+//        mAdapter.setOnImageSelectListener(new ImageAdapter.OnImageSelectListener() {
+//            @Override
+//            public void OnImageSelect(Image image, boolean isSelect, int selectCount) {
+//                setSelectImageCount(selectCount);
+//            }
+//        });
+//        mAdapter.setOnItemClickListener(new ImageAdapter.OnItemClickListener() {
+//            @Override
+//            public void OnItemClick(Image image, int position) {
+//                toPreviewActivity(mAdapter.getData(), position);
+//            }
+//        });
     }
 }
