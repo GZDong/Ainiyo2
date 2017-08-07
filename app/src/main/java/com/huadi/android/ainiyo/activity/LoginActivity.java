91790ed95fc112aabfa1fff660df439fed44722c
xiaoxing <190018588@qq.com>
2017-08-07 23:11:49 +0800

    little change

--- app/src/main/java/com/huadi/android/ainiyo/activity/LoginActivity.java
+++ app/src/main/java/com/huadi/android/ainiyo/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -113,5 +114,12 @@ public class LoginActivity extends AppCompatActivity  {
                         }
                 );
         }
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+            //不写东西，按下返回键就没操作
+        }
+        return false;
     }
 }
