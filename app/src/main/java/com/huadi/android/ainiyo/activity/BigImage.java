9195d35364e1bbe3d127e927e60b3fc92613a053
xiaoxing <190018588@qq.com>
2017-08-16 16:01:25 +0800

    做了简易的显示图片的相册

---
+++ app/src/main/java/com/huadi/android/ainiyo/activity/BigImage.java
@@ -0,0 +1,30 @@
+package com.huadi.android.ainiyo.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.huadi.android.ainiyo.R;
+
+public class BigImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_big_image);
+        Intent intent=getIntent();
+        String url=intent.getStringExtra("URL");
+        ImageView imageView=(ImageView)findViewById(R.id.big);
+        Glide.with(BigImage.this).load(url).placeholder(R.drawable.on).error(R.drawable.fail).into(imageView);
+        imageView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/huadi/android/ainiyo/adapter/MyAdapter.java
+++ app/src/main/java/com/huadi/android/ainiyo/adapter/MyAdapter.java
@@ -1,5 +1,6 @@
 package com.huadi.android.ainiyo.adapter;
 
+import android.content.Intent;
 import android.widget.ImageView;
 
 
@@ -10,9 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.huadi.android.ainiyo.R;
+import com.huadi.android.ainiyo.activity.BigImage;
+import com.huadi.android.ainiyo.activity.PhotoActivity;
 
 
 import java.util.List;
@@ -21,10 +25,21 @@ import java.util.List;
  * Created by 45990 on 2017/8/13.
  */
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHodler> {
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     private Context mContext;
     private List<String> mList;
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+      ImageView mImageView;
+        View img;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            img=itemView;
+            mImageView= (ImageView) itemView.findViewById(R.id.im_photo);
+        }
+    }
 
     public MyAdapter(Context context, List<String> data) {
         mContext = context;
@@ -32,17 +47,26 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHodler> {
     }
 
     @Override
-    public ViewHodler onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         View view= LayoutInflater.from(mContext).inflate(R.layout.adapter_image_item,parent,false);
-
-        return new ViewHodler(view);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(ViewHodler holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, int position) {
 
         Glide.with(mContext).load(mList.get(position)).into(holder.mImageView);
+        holder.img.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                int position=holder.getAdapterPosition();
+                String url=mList.get(position);
+                Intent intent=new Intent(mContext,BigImage.class);
+                intent.putExtra("URL",url);
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -50,13 +74,5 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHodler> {
         return mList.size();
     }
 
-    public class ViewHodler extends RecyclerView.ViewHolder{
 
-        private ImageView mImageView;
-
-        public ViewHodler(View itemView) {
-            super(itemView);
-            mImageView= (ImageView) itemView.findViewById(R.id.im_photo);
-        }
-    }
 }
