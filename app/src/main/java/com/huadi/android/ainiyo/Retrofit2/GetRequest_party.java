44eaa74a1914f3338356ab4481a295cdc52ecab2
rxvincent <rxvincent@starvines.com>
2017-08-18 10:18:09 +0800

    无效化了无用文件

--- app/src/main/java/com/huadi/android/ainiyo/Retrofit2/GetRequest_party.java
+++ app/src/main/java/com/huadi/android/ainiyo/Retrofit2/GetRequest_party.java
@@ -1,22 +1,22 @@
-package com.huadi.android.ainiyo.Retrofit2;
-
-import com.huadi.android.ainiyo.activity.MovementDetailActivity;
-import com.huadi.android.ainiyo.gson.ResultForCheckName;
-
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.Path;
-import rx.Observable;
-
-/**
- * Created by rxvincent on 2017/8/14.
- */
-
-public interface GetRequest_party {
-    @GET("party/{id}")
-    Observable<MovementDetailActivity.PartyInformation> getPartyInformation(@Field("sessionid") String sessionid, @Field("id") String id);
-}
+//package com.huadi.android.ainiyo.Retrofit2;
+//
+//import com.huadi.android.ainiyo.activity.MovementDetailActivity;
+//import com.huadi.android.ainiyo.gson.ResultForCheckName;
+//
+//import okhttp3.ResponseBody;
+//import retrofit2.Call;
+//import retrofit2.http.Field;
+//import retrofit2.http.FormUrlEncoded;
+//import retrofit2.http.GET;
+//import retrofit2.http.POST;
+//import retrofit2.http.Path;
+//import rx.Observable;
+//
+///**
+// * Created by rxvincent on 2017/8/14.
+// */
+//
+//public interface GetRequest_party {
+//    @GET("party/{id}")
+//    Observable<MovementDetailActivity.PartyInformation> getPartyInformation(@Field("sessionid") String sessionid, @Field("id") String id);
+//}
