46565ddb01de00fe83bd5cb0e4072d945864ceb6
fengsam <404395182@qq.com>
2017-08-20 22:23:01 +0800

    finding页面增加脱机显示

--- app/src/main/java/com/huadi/android/ainiyo/frag/FindingFragment.java
+++ app/src/main/java/com/huadi/android/ainiyo/frag/FindingFragment.java
@@ -210,6 +210,17 @@ public class FindingFragment extends Fragment {
 
                     initView();
 
+                } else {
+                    FindingInfo fi1 = new FindingInfo("1", 0.60f, 0.94f, 0.90f, 0.80f, 0.70f, 0.30f, 0.33f, 0.94f, "刘奕宁1", true, "123", 20, "学生");
+                    FindingInfo fi2 = new FindingInfo("1", 0.90f, 0.60f, 0.80f, 0.40f, 0.77f, 0.90f, 0.80f, 0.40f, "刘奕宁2", true, "123", 20, "教师");
+                    FindingInfo fi3 = new FindingInfo("1", 0.90f, 0.60f, 0.80f, 0.40f, 0.77f, 0.90f, 0.80f, 0.40f, "刘奕宁3", true, "123", 20, "教师");
+                    FindingInfo fi4 = new FindingInfo("1", 0.90f, 0.60f, 0.80f, 0.40f, 0.77f, 0.90f, 0.80f, 0.40f, "刘奕宁4", true, "123", 20, "教师");
+
+                    mList.add(fi1);
+                    mList.add(fi2);
+                    mList.add(fi3);
+                    mList.add(fi4);
+                    initView();
                 }
             }
 
