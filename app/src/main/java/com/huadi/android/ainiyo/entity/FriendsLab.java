61621ebe17f7e798c355a5002844d47593c1afb3
gaozhidong <dong_scau@163.com>
2017-09-01 09:56:51 +0800

    修复一个小问题

--- app/src/main/java/com/huadi/android/ainiyo/entity/FriendsLab.java
+++ app/src/main/java/com/huadi/android/ainiyo/entity/FriendsLab.java
@@ -62,6 +62,7 @@ public class FriendsLab {
     private Map<String,Date> keepTime;
     private Map<String,Integer> keepUnread;
     private Map<String,String> keepLastMsg;
+    private Map<String,Boolean> keepConversation;
 
     private int isRequsetNewInfo = 1;
     private String NewFriName = null;
@@ -173,6 +174,11 @@ public class FriendsLab {
                                                     if (date!=null){
                                                         friends.setDate(date);
                                                     }
+                                                }
+
+                                                if (keepConversation!=null){
+                                                    boolean isShow = keepConversation.get(friends.getFriId());
+                                                    friends.setShowInChooseFragment(isShow);
                                                 }
 
                                                 if (keepUnread!=null){
@@ -425,11 +431,13 @@ public class FriendsLab {
         keepTime = new HashMap<>();
         keepUnread = new HashMap<>();
         keepLastMsg = new HashMap<>();
+        keepConversation = new HashMap<>();
         DataSupport.deleteAll(Friends.class,"user = ?",mUserInfo.getUsername());
         for (Friends friends : mFriendses){
             keepTime.put(friends.getFriId(),friends.getDate());
             keepUnread.put(friends.getFriId(),friends.getUnreadMeg());
             keepLastMsg.put(friends.getFriId(),friends.getLastMsg());
+            keepConversation.put(friends.getFriId(),friends.isShowInChooseFragment());
         }
         mFriendses = null;
         initFriends();
